681d0da6da966b783cd9b66338f535c4718eab52
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2018-10-07 07:30:06 -0700

    transfer

--- index.js
+++ index.js
@@ -1,26 +1,61 @@
 
 
-const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-const asciiToHex = Web3.utils.asciiToHex;
-const contractInstance = new web3.eth.Contract(ABI_DEFINITION, CONTRACT_ADDRESS);
-
+let contractInstance = null;
 let userAddress = null;
-web3.eth.getAccounts()
-.then((accounts) => {
-  userAddress = accounts[2]
+let balance = null;
+
+window.addEventListener('load', function() {
+  console.log("loading");
+
+  if (typeof web3 !== 'undefined') {
+  console.log("starting");
+  userAddress = web3.eth.defaultAccount;
+  web3js = new Web3(web3.currentProvider);
+  contractInstance = new web3js.eth.Contract(abi, address);
+
+   updateBalance(userAddress);
+   updateSellReserve();
+   updateNumTokens();
+
+  } else {
+     // Warn the user that they need to get a web3 browser
+     // Or install MetaMask, maybe with a nice graphic.
+  console.warn("need metamask");
+  }
 })
-.then(function() {
-  updateBalance(userAddress);
-  updateSellReserve();
-  updateNumTokens();
-});
 
 
 function buy() {
   const value = $("#buy-value").val();
-  contractInstance.methods.minting().send({from: userAddress,
-                                           value: value,
-                                           gas:100000})
+  wei = web3.toWei(value, 'ether');
+  transaction = contractInstance.methods.buy().send({
+                    'from': userAddress,
+                    'gas': 4712388,
+                    'gasPrice': 100000,
+                    'value': wei
+    })
+    .then(function() {
+      updateBalance(userAddress);
+      updateSellReserve();
+      updateNumTokens();
+    });
+}
+    // tx_receipt = w3.eth.getTransactionReceipt(tx_hash)['contractAddress']
+        // return tx_receipt
+
+  //  console.log('buy', userAddress);
+  // const value = $("#buy-value").val();
+  // contractInstance.methods.buy().send({from: userAddress,
+  //                                          value: value,
+  //                                          gas:100000})
+  //
+// }
+
+
+
+function sell() {
+  const value = $("#sell-value").val();
+  contractInstance.methods.sell(value).send({from: userAddress,gas:100000})
   .then(function() {
     updateBalance(userAddress);
     updateSellReserve();
@@ -28,10 +63,15 @@ function buy() {
   });
 }
 
-
-function sell() {
-  const value = $("#sell-value").val();
-  contractInstance.methods.burning(value).send({from: userAddress,gas:100000})
+function transfer() {
+  const value = $("#transfer-amount").val();
+  wei = web3.toWei(value, 'ether');
+  const address = $("#transfer-address").val();
+  contractInstance.methods.transfer(address, wei).send({
+    from: userAddress,
+    gas: 4712388,
+    gasPrice: 100000
+  })
   .then(function() {
     updateBalance(userAddress);
     updateSellReserve();
@@ -40,23 +80,24 @@ function sell() {
 }
 
 function updateBalance(address) {
-  contractInstance.methods.getBalance().call({from:address})
-  .then((balance) => {
-    $("#balance").html(balance);
+  contractInstance.methods.balanceOf(address).call()
+  .then((res) => {
+    balance = parseFloat(web3.fromWei(res,"ether"));
+    $("#balance").html(balance.toFixed(3));
   });
 }
 
 function updateSellReserve() {
-  contractInstance.methods.getSellReserve().call()
+  contractInstance.methods.sellReserve().call()
   .then((res) => {
-    $("#sell-reserve").html(parseInt(res)/1000);
+    $("#sell-reserve").html(parseFloat(web3.fromWei(res)).toFixed(3));
   });
 }
 
 function updateNumTokens() {
-  contractInstance.methods.getNumTokens().call()
+  contractInstance.methods.totalSupply().call()
   .then((res) => {
-    $("#n-tokens").html(res);
+    $("#n-tokens").html(parseFloat(web3.fromWei(res)).toFixed(3));
   });
 }
 
