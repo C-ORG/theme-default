2a6069c014952dbec323c49223fb692c2f3ff9e3
Pierre-Louis Guhur <pierre-louis.guhur@laposte.net>
2018-10-07 07:56:35 -0700

    add revenue

--- index.js
+++ index.js
@@ -10,15 +10,16 @@ window.addEventListener('load', function() {
   if (typeof web3 !== 'undefined') {
   console.log("starting");
   userAddress = web3.eth.defaultAccount;
-  web3js = new Web3(web3.currentProvider);
-  contractInstance = new web3js.eth.Contract(abi, address);
+  web1 = new Web3(web3.currentProvider);
+  web0 = web3;
+  contractInstance = new web1.eth.Contract(abi, address);
 
    updateBalance(userAddress);
    updateSellReserve();
    updateNumTokens();
 
   } else {
-     // Warn the user that they need to get a web3 browser
+     // Warn the user that they need to get a web0 browser
      // Or install MetaMask, maybe with a nice graphic.
   console.warn("need metamask");
   }
@@ -27,7 +28,7 @@ window.addEventListener('load', function() {
 
 function buy() {
   const value = $("#buy-value").val();
-  wei = web3.toWei(value, 'ether');
+  wei = web0.toWei(value, 'ether');
   transaction = contractInstance.methods.buy().send({
                     'from': userAddress,
                     'gas': 4712388,
@@ -40,22 +41,16 @@ function buy() {
       updateNumTokens();
     });
 }
-    // tx_receipt = w3.eth.getTransactionReceipt(tx_hash)['contractAddress']
-        // return tx_receipt
-
-  //  console.log('buy', userAddress);
-  // const value = $("#buy-value").val();
-  // contractInstance.methods.buy().send({from: userAddress,
-  //                                          value: value,
-  //                                          gas:100000})
-  //
-// }
-
 
 
 function sell() {
   const value = $("#sell-value").val();
-  contractInstance.methods.sell(value).send({from: userAddress,gas:100000})
+  wei = web0.toWei(value, 'ether');
+  contractInstance.methods.sell(wei).send({
+    from: userAddress,
+    gas: 4712388,
+    gasPrice: 100000
+  })
   .then(function() {
     updateBalance(userAddress);
     updateSellReserve();
@@ -65,7 +60,7 @@ function sell() {
 
 function transfer() {
   const value = $("#transfer-amount").val();
-  wei = web3.toWei(value, 'ether');
+  wei = web0.toWei(value, 'ether');
   const address = $("#transfer-address").val();
   contractInstance.methods.transfer(address, wei).send({
     from: userAddress,
@@ -79,25 +74,65 @@ function transfer() {
   });
 }
 
+function transfer() {
+  const value = $("#transfer-amount").val();
+  wei = web0.toWei(value, 'ether');
+  const address = $("#transfer-address").val();
+  contractInstance.methods.transfer(address, wei).send({
+    from: userAddress,
+    gas: 4712388,
+    gasPrice: 100000
+  })
+  .then(function() {
+    updateBalance(userAddress);
+    updateSellReserve();
+    updateNumTokens();
+  });
+}
+
+
+function revenue() {
+  const value = $("#revenue-value").val();
+  wei = web0.toWei(value, 'ether');
+  contractInstance.methods.revenue().send({
+    from: userAddress,
+    gas: 4712388,
+    gasPrice: 100000,
+    value: wei
+  })
+  .then(function() {
+    updateBalance(userAddress);
+    updateSellReserve();
+    updateNumTokens();
+  });
+}
+
+
 function updateBalance(address) {
   contractInstance.methods.balanceOf(address).call()
   .then((res) => {
-    balance = parseFloat(web3.fromWei(res,"ether"));
-    $("#balance").html(balance.toFixed(3));
+    balance = parseFloat(web0.fromWei(res,"ether"));
+    $("#balance-tok").html(balance.toFixed(3));
+  });
+
+  web1.eth.getBalance(address)
+  .then((res) => {
+    balance = parseFloat(web0.fromWei(res,"ether"));
+    $("#balance-eth").html(balance.toFixed(3));
   });
 }
 
 function updateSellReserve() {
   contractInstance.methods.sellReserve().call()
   .then((res) => {
-    $("#sell-reserve").html(parseFloat(web3.fromWei(res)).toFixed(3));
+    $("#sell-reserve").html(parseFloat(web0.fromWei(res)).toFixed(3));
   });
 }
 
 function updateNumTokens() {
   contractInstance.methods.totalSupply().call()
   .then((res) => {
-    $("#n-tokens").html(parseFloat(web3.fromWei(res)).toFixed(3));
+    $("#n-tokens").html(parseFloat(web0.fromWei(res)).toFixed(3));
   });
 }
 
